538983006cdb79d0706c8fed6ee813c100351f9d
ajiprasetyo471 <ajiprsty4713@gmail.com>
2023-08-17 17:00:25 +0700

    disable country drop

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -58,7 +58,7 @@
             <v-icon right dark> mdi-menu-down </v-icon>
           </v-btn>
         </template>
-        <v-list>
+        <!-- <v-list>
           <v-list-item
             v-for="(item, index) in country"
             :key="index"
@@ -67,7 +67,7 @@
           >
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
-        </v-list>
+        </v-list> -->
       </v-menu>
     </div>
     <v-btn v-if="!isWelcome" elevation="0" class="btn_sign__up" to="/welcome">
